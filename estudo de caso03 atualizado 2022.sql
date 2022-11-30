7479f508b9753deb21e21bbbeee888c61af7001e
joao <jmsmarchi@gmail.com>
2022-11-30 09:07:14 -0300

    final

--- estudo de caso03 atualizado 2022.sql
+++ estudo de caso03 atualizado 2022.sql
@@ -1,4 +1,4 @@
-DROP DATABASE skateshop ; 
+ DROP DATABASE skateshop ;
 CREATE DATABASE skateshop;
 USE skateshop;
 
@@ -32,24 +32,20 @@ CREATE TABLE fornecedores (
   );
 
 CREATE TABLE itens_vendidos (
-  id_produto INT NOT NULL,
-  valor_produto_vendido INT NOT NULL,
-  nome_produto VARCHAR(45) NOT NULL,
-  cliente_cpf VARCHAR(14) NOT NULL,
-  PRIMARY KEY ( id_produto, cliente_cpf),
-    FOREIGN KEY (cliente_cpf) REFERENCES cliente (cpf)
+  id_produto INT NOT NULL auto_increment,
+  valor_produto_vendido  VARCHAR(45)  NOT NULL,
+  nome_produto VARCHAR(256) NOT NULL,
+  PRIMARY KEY ( id_produto)
+  
 );
 
 
 
 CREATE TABLE pecas (
   id_pecas INT NOT NULL auto_increment,
-  valor FLOAT NOT NULL,
-  categoria_id_categoria INT NOT NULL,
-  fornecedores_id_fornecedor INT NOT NULL,
-  PRIMARY KEY (id_pecas, categoria_id_categoria, fornecedores_id_fornecedor),
-    FOREIGN KEY (categoria_id_categoria) REFERENCES categoria (id_categoria),
-    FOREIGN KEY (fornecedores_id_fornecedor) REFERENCES fornecedores (id_fornecedor)
+  nome_pecas VARCHAR(246) NOT NULL,
+  valor VARCHAR(46) NOT NULL,
+  PRIMARY KEY (id_pecas)
  );
 
 CREATE TABLE itens_pedido (
@@ -63,6 +59,20 @@ CREATE TABLE itens_pedido (
   
   
   
+  INSERT INTO pecas (nome_pecas,valor)
+  VALUES
+  ('SHAPE ELEMENT 8.250 SECTION','R$ 299,96'),
+  ('SHAPE ELEMENT 8.250 PEARL WWFE','R$ 359,99'),
+  ('SHAPE ELEMENT 8.375 QUADRANT','R$ 400,99'),
+  ('SHAPE AHEAD 8.0 TIE DYE CLOUD','R$ 299,'),
+  ('SHAPE AHEAD 8.0 TIE DYE 70','R$ 129,88'),
+  ('SHAPE CISCO 8.0 MAPLE','R$ 339,60'),
+  ('SHAPE SANTA CRUZ 7.9 ','R$ 359,70'),
+  ('RODA ELEMENT 39MM ','R$ 299,'),
+  ('LIXA CISCO EMBORACHADA ','R$ 69,30'),
+  ('SHAPE ELEMENT 7.8 ','R$ 456,99');
+
+
 
 INSERT INTO cliente (cpf, nome, email, telefone)
 VALUES
@@ -91,6 +101,7 @@ VALUES
 ( 'PROFISSIONAL','RODA RICTA 53MM WIREFRAME SPARX WHITE 99A');
 
 
+
 INSERT INTO fornecedores (nome_do_fornecedor,CEP)
 VALUES
 ('Felipe Martins','58808-255'),
@@ -105,14 +116,24 @@ VALUES
 ('Júlia Ramos','29101-568');
 
 
-
-
-
-  /*
-INSERT INTO itens_vendidos (id_produto, valor_produto_vendido, nome_produto )
+INSERT INTO itens_vendidos ( valor_produto_vendido, nome_produto )
 VALUES
-(1,2,'SHAPE AHEAD 8.0 TIE DYE CLOUD');
-*/
+('R$258.78','SHAPE AHEAD 8.0 TIE DYE' ),
+('R$649,90','TRUCK INDEPENDENT 139MM STAGE STANDARD 11 PAR' ),
+('R$788.98','TRUCK INDEPENDENT 149MM STAGE 11 STANDARD - PAR'),
+('R$ 129,90','SHAPE SDS CO 8.0 CRANIAL BASE' ),
+('R$448.77','SHAPE ELEMENT 8.250 SECTION' ),
+('R$458.98','Shape Element 8.250 Combine Gabriel') ,
+('R$366.99','RODA RICTA 52MM FRAMEWORK SPARX WHITE 99A' ),
+('R$158.65','RODA RICTA 53MM FREAMEWORK SPARX BLACK 99A' ),
+('R$69.68','LIXA EMBORRACHADA BLACK SHEEP BICICLETINHA' ),
+('R$58.74','Lixa Emborrachada Black Sheep Granulado' );
+
+UPDATE itens_vendidos SET  nome_produto='SHAPE AHEAD 8.25 TIE DYE' WHERE id_produto=1;
+UPDATE itens_vendidos SET  nome_produto='TRUCK INDEPENDENT 149MM STAGE 12 STANDARD - PAR' WHERE id_produto=3;
+UPDATE itens_vendidos SET  valor_produto_vendido= 'R$548.77' WHERE id_produto=5;
+UPDATE itens_vendidos SET  valor_produto_vendido= 'R$469.99' WHERE id_produto=7;
+UPDATE itens_vendidos SET  nome_produto='Lixa Emborrachada Independent Granulado' WHERE id_produto=10;
 
 UPDATE cliente SET nome='Genilda Maria da Silva Keirox' WHERE cpf ='427.450.394-15';
 UPDATE cliente SET email='newotavin@yahoo.com.b' WHERE cpf ='877.109.980-89';
@@ -126,11 +147,19 @@ UPDATE categoria SET nome_categoria='INICIANTE' WHERE id_categoria =7;
 UPDATE categoria SET nome_categoria='PROFISSIONAL' WHERE id_categoria =6;
 UPDATE categoria SET nome_categoria='INTERMEDIARIO' WHERE id_categoria =5;
 
+
 UPDATE fornecedores SET nome_do_fornecedor='Pedro Martins' WHERE id_fornecedor=1;
 UPDATE fornecedores SET nome_do_fornecedor='Carlos Alberto' WHERE id_fornecedor=4;
 UPDATE fornecedores SET CEP='68850-115' WHERE id_fornecedor=8;
 UPDATE fornecedores SET CEP='48850-835' WHERE id_fornecedor=10;
 UPDATE fornecedores SET nome_do_fornecedor='Alrbet Albin' WHERE id_fornecedor=6;
+
+DELETE FROM itens_vendidos WHERE id_produto=2;
+DELETE FROM itens_vendidos WHERE id_produto=4;
+DELETE FROM itens_vendidos WHERE id_produto=6;
+DELETE FROM itens_vendidos WHERE id_produto=8;
+DELETE FROM itens_vendidos WHERE id_produto=9;
+
 
 DELETE FROM categoria WHERE id_categoria=6;
 DELETE FROM categoria WHERE id_categoria=3;
@@ -150,13 +179,21 @@ DELETE FROM cliente WHERE cpf = '715.616.000-16';
 DELETE FROM cliente WHERE cpf = '280.185.120-51';
 DELETE FROM cliente WHERE cpf = '289.727.630-47';
 
+DELETE FROM pecas WHERE id_pecas=2;
+DELETE FROM pecas WHERE id_pecas=3;
+DELETE FROM pecas WHERE id_pecas=2;
+DELETE FROM pecas WHERE id_pecas=6;
+DELETE FROM pecas WHERE id_pecas=7;
+
 select * from fornecedores ORDER BY id_fornecedor;
 select * from categoria ORDER BY id_categoria;
 select * from cliente ORDER BY cpf;
+select * from itens_vendidos ORDER BY id_produto;
+select * from pecas ORDER BY id_pecas;
 
 
 select * from fornecedores WHERE id_fornecedor=8;
-
 select * from cliente WHERE cpf = '543.982.940-74';
-
 select * from categoria WHERE id_categoria=1;
+select * from itens_vendidos WHERE id_produto=1;
+select * from pecas WHERE id_pecas=1;
